8ccc87ae1019dc5c1c06abc491d55be469da9b1a
Trong Nguyen <nguyenphutrong.dev@gmail.com>
2026-06-03 20:14:36 +0700

    test: fix cpa-plusplus homebrew test

--- Formula/cpa-plusplus.rb
+++ Formula/cpa-plusplus.rb
@@ -35,6 +35,6 @@ class CpaPlusplus < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/cpa-plusplus --version")
+    assert_match version.to_s, shell_output("#{bin}/cpa-plusplus --help")
   end
 end
